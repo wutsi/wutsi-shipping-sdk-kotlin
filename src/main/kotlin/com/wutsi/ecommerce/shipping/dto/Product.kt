4a014fcc12123c609eec14c8e0c2cced0f857376
wutsi <herve.tchepannou@wutsi.com>
2022-05-23 20:25:28 -0400

    update api

--- src/main/kotlin/com/wutsi/ecommerce/shipping/dto/Product.kt
+++ src/main/kotlin/com/wutsi/ecommerce/shipping/dto/Product.kt
@@ -1,9 +1,11 @@
 package com.wutsi.ecommerce.shipping.dto
 
+import kotlin.Int
 import kotlin.Long
 import kotlin.String
 
 public data class Product(
   public val productId: Long = 0,
-  public val productType: String = ""
+  public val productType: String = "",
+  public val quantity: Int = 0
 )
