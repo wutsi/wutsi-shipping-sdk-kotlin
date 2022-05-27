78fcb376cdcff0e5c8f799a94494dfcca647fd96
wutsi <herve.tchepannou@wutsi.com>
2022-05-26 20:53:40 -0400

    add shipping order

--- src/main/kotlin/com/wutsi/ecommerce/shipping/WutsiShippingApi.kt
+++ src/main/kotlin/com/wutsi/ecommerce/shipping/WutsiShippingApi.kt
@@ -1,7 +1,11 @@
 package com.wutsi.ecommerce.shipping
 
+import com.wutsi.ecommerce.shipping.dto.ChangeStatusRequest
+import com.wutsi.ecommerce.shipping.dto.CreateShippingOrderRequest
+import com.wutsi.ecommerce.shipping.dto.CreateShippingOrderResponse
 import com.wutsi.ecommerce.shipping.dto.CreateShippingRequest
 import com.wutsi.ecommerce.shipping.dto.CreateShippingResponse
+import com.wutsi.ecommerce.shipping.dto.GetShippingOrderResponse
 import com.wutsi.ecommerce.shipping.dto.GetShippingResponse
 import com.wutsi.ecommerce.shipping.dto.ListShippingResponse
 import com.wutsi.ecommerce.shipping.dto.SearchRateRequest
@@ -38,4 +42,16 @@ public interface WutsiShippingApi {
   @RequestLine("POST /v1/rates/search")
   @Headers(value=["Content-Type: application/json"])
   public fun searchRate(request: SearchRateRequest): SearchRateResponse
+
+  @RequestLine("POST /v1/shipping-orders")
+  @Headers(value=["Content-Type: application/json"])
+  public fun createShippingOrder(request: CreateShippingOrderRequest): CreateShippingOrderResponse
+
+  @RequestLine("GET /v1/shipping-orders/{id}")
+  @Headers(value=["Content-Type: application/json"])
+  public fun getShippingOrder(@Param("id") id: Long): GetShippingOrderResponse
+
+  @RequestLine("POST /v1/shipping-orders/{id}/status")
+  @Headers(value=["Content-Type: application/json"])
+  public fun changeShippingOrderStatus(@Param("id") id: Long, request: ChangeStatusRequest): Unit
 }
---
+++ src/main/kotlin/com/wutsi/ecommerce/shipping/dto/CreateShippingOrderRequest.kt
@@ -0,0 +1,7 @@
+package com.wutsi.ecommerce.shipping.dto
+
+import kotlin.String
+
+public data class CreateShippingOrderRequest(
+  public val orderId: String = ""
+)
